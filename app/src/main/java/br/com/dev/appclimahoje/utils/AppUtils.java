3eabaf6418b02e0918beb6e177e90d5da603b483
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-11-04 00:14:10 -0300

    Estruturando telas

--- app/src/main/java/br/com/dev/appclimahoje/utils/AppUtils.java
+++ app/src/main/java/br/com/dev/appclimahoje/utils/AppUtils.java
@@ -4,5 +4,21 @@ public class AppUtils {
 
     public static final String TAG = "LOG_APPCLIMAHOJE";
     public static final int TIME_SPLASH = 4 * 1000;
+    public static final String PREF = "appclimahojepref";
 
+    /**
+     * Função para validar se senha digita corresponde com a senha cadastrada
+     * @return
+     */
+    public boolean validarSenha(){
+        return true;
+    }
+
+    /**
+     * Função criada para validação de Email
+     * @return
+     */
+    public boolean valirdarEmail(){
+        return true;
+    }
 }
