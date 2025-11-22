d0303ef36a0024cb6a5d72b136a1314db577ece8
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2025-11-21 21:37:39 -0300

    Incrementando a classe AppUtils

--- app/src/main/java/br/com/dev/appclimahoje/utils/AppUtils.java
+++ app/src/main/java/br/com/dev/appclimahoje/utils/AppUtils.java
@@ -1,9 +1,14 @@
 package br.com.dev.appclimahoje.utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,7 +16,8 @@ public class AppUtils {
 
     public static final String TAG = "LOG_APPCLIMAHOJE";
     public static final int TIME_SPLASH = 4 * 1000;
-    public static final String PREF = "appclimahojepref";
+    public static final String PREF = "app_clima_hoje_pref";
+    public static final String VERSION = "v1.0.0";
 
     private static final String EMAIL_PADRAO = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
     private static final Pattern padraoAceito = Pattern.compile(EMAIL_PADRAO,Pattern.CASE_INSENSITIVE);
@@ -20,7 +26,8 @@ public class AppUtils {
     //private static final String SENHA_FORTE = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{10,}$";
     private static final Pattern padraoSenhaSimples = Pattern.compile(SENHA_SIMPLES);
 
-
+    public static SharedPreferences pref;
+    public static Context context;
 
     /**
      * Função para validar se senha digitada corresponde com a senha cadastrada
@@ -45,8 +52,15 @@ public class AppUtils {
      * Método para validar se a senha digitada no Login é válida
      * @return
      */
-    public boolean validaSenhaLogin(){
-        return true;
+    public boolean validaSenhaLogin(String senhaInformada){
+        pref = context.getSharedPreferences(PREF,Context.MODE_PRIVATE);
+
+        if(pref.getString("senha",null).equals(senhaInformada)){
+            return true;
+        }
+
+        return false;
+
     }
 
     /**
@@ -92,5 +106,22 @@ public class AppUtils {
             default:
                 Log.d(TAG, mensagem);
         }
+    }
+
+    /**
+     *
+     * @return Retorna a data no formato "dd/MM/yyyy 00:00:00"
+     */
+    public static String getDataFormat(){
+
+        String dataAtualFormat;
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+
+        dataAtualFormat = dateFormat.format(date);
+
+        return dataAtualFormat;
+
     }
 }
